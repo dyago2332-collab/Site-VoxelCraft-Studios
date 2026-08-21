ff90e289934a1f22e6c3c3e159a313fb35adf5c1
dyago2332-collab <dyago2332@gmail.com>
2026-08-20 22:05:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Meu site Python está no ar pelo Google!</h1>"
+    # Abre o seu arquivo index.html na página inicial
+    return send_from_directory('.', 'index.html')
+
+@app.route('/<path:path>')
+def send_static(path):
+    # Carrega as imagens, estilos e scripts das suas pastas
+    return send_from_directory('.', path)
 
 if __name__ == "__main__":
-    # O Google Cloud define a porta automaticamente através da variável PORT
     port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
